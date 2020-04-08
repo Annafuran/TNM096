069135491f463f93fd9e0b6687763903919773fc
Annafuran <33253216+Annafuran@users.noreply.github.com>
2020-04-08 12:53:13 +0200

    error

--- Lab1/Lab1/Lab1.py
+++ Lab1/Lab1/Lab1.py
@@ -1,8 +1,18 @@
 import numpy as np
 import random
 
+#GLOBAL VARIABLE
+
+
+def changeGrandParent(newGrandParent):
+
+    grandparent = newGrandParent
+
+    return grandparent
+   
+
 #Calculating the herustic cost 
-def heruCost(tempNode, nodeList):
+def heruCost(tempNode, nodeList, deepCounter):
     GoalNode = ["1", "2", "3", "4", "5", "6", "7", "8", "0"]
     heruMis = 0
 
@@ -10,12 +20,14 @@ def heruCost(tempNode, nodeList):
         if tempNode[i] != GoalNode[i]:
             heruMis = heruMis +1
     
+    # f = h + g
+    f = heruMis
     #Adding the herustic cost to the list of costs
-    nodeList.append(heruMis)
+    nodeList.append(f)
     return nodeList
 
 #Finding all the possible moves
-def possibleMoves(NodeList):
+def possibleMoves(NodeList, deepCounter):
     ZeroLocation = NodeList.index("0")
     tempNode=[]
     tempNode.extend(NodeList)
@@ -26,7 +38,7 @@ def possibleMoves(NodeList):
         tempNode[NodeList.index("0")] = tempNode[NodeList.index("0")+3]
         tempNode[NodeList.index("0")+3] = temp
         #Saving the heuristic cost in a list 
-        CostList = heruCost(tempNode, CostList)
+        CostList = heruCost(tempNode, CostList, deepCounter)
         tempNode=[]
         tempNode.extend(NodeList)
     else:
@@ -37,7 +49,7 @@ def possibleMoves(NodeList):
        tempNode[NodeList.index("0")] = tempNode[NodeList.index("0")-3]
        tempNode[NodeList.index("0")-3] = temp
        #Saving the heuristic cost in a list 
-       CostList = heruCost(tempNode, CostList)
+       CostList = heruCost(tempNode, CostList, deepCounter)
        tempNode=[]
        tempNode.extend(NodeList)
     else:
@@ -48,7 +60,7 @@ def possibleMoves(NodeList):
        tempNode[NodeList.index("0")] = tempNode[NodeList.index("0")-1]
        tempNode[NodeList.index("0")-1] = temp
        #Saving the heuristic cost in a list 
-       CostList = heruCost(tempNode, CostList)
+       CostList = heruCost(tempNode, CostList, deepCounter)
        tempNode=[]
        tempNode.extend(NodeList)
     else:
@@ -59,67 +71,80 @@ def possibleMoves(NodeList):
        tempNode[NodeList.index("0")] = tempNode[NodeList.index("0")+1]
        tempNode[NodeList.index("0")+1] = temp
        #Saving the heuristic cost in a list 
-       CostList = heruCost(tempNode, CostList)
+       CostList = heruCost(tempNode, CostList, deepCounter)
        tempNode=[]
        tempNode.extend(NodeList)
     else:
         CostList.append(1000)
     return CostList
 
-def moveNode(StartNode, CostList):
+def moveNode(ClosedList, CostList, Visited, deepCounter, grandparent):
+
+    OpenList = []
     
     def swapPositions(list, pos1, pos2):
         list[pos1], list[pos2] = list[pos2], list[pos1]
-        return list
+        resultinglist = list
+        return resultinglist
     
     def switching(argument):
         switcher = {
-        0: StartNode.index("0")+3,
-        1: StartNode.index("0")-3,
-        2: StartNode.index("0")-1,
-        3: StartNode.index("0")+1          
+        0: ClosedList.index("0")+3,
+        1: ClosedList.index("0")-3,
+        2: ClosedList.index("0")-1,
+        3: ClosedList.index("0")+1          
         }
         return switcher.get(minIndex, "Invalid value")
     
-    minIndex = CostList.index(min(CostList))
-    switchLocation = switching(minIndex)
-    ZeroLocation = StartNode.index("0")
-   
-    StartNode = swapPositions(StartNode, ZeroLocation, switchLocation)  
+    while True:
+        minIndex = CostList.index(min(CostList))
+        switchLocation = switching(minIndex)
+        ZeroLocation = ClosedList.index("0")
+        OpenList = swapPositions(ClosedList, ZeroLocation, switchLocation)
+        print("hej")
+        print(ZeroLocation)
+        print(OpenList)
+        print(ClosedList)
+        if OpenList == grandparent:
+            CostList[minIndex] = 1000;
+        else:           
+            grandparent = ClosedList
+            ClosedList = OpenList;
+            break
+    
+    return ClosedList, grandparent
 
-    print(StartNode)
 
-    return StartNode
-
-
-def puzzleGame(StartNode):
+def puzzleGame(ClosedList, GoalNode):
     counter = 0
+    deepCounter = 0
     costList = []
-    puzzelList = StartNode
+    puzzelList = ClosedList
+    visited = [12]
+    grandparent = []
 
     while True:
-        if StartNode == None:
+        if ClosedList == None:
             print("No solution")
+            break
+        if  ClosedList == GoalNode:
+            print("End of game")
+            break
         else:
-            costList = possibleMoves(puzzelList)
-            puzzelList = moveNode(puzzelList, costList)
+            costList = possibleMoves(puzzelList, deepCounter)
+            puzzelList, grandparent = moveNode(puzzelList, costList, visited, deepCounter, grandparent)
+            deepCounter += 1
 
     return puzzelList
 
 
-            #Kolla möjliga moves
-
-            #Kolla vilken som är billigast
-
-            #Välja next move
-
-
-StartNode =  ["1", "2", "3", "4", "5", "6", "7", "8", "0"]
+ClosedList =  ["3", "1", "6", "8", "4", "5", "7", "2", "0"]
+GoalNode =  ["1", "2", "3", "4", "5", "6", "7", "8", "0"]
 resultingGame = []
 
-random.shuffle(StartNode)
+#random.shuffle(ClosedList)
 
-resultingGame = puzzleGame(StartNode) 
+resultingGame = puzzleGame(ClosedList, GoalNode) 
 
 print(resultingGame)
     
